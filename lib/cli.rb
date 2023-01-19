7ad117d5238eeb4a7bacfb5586571aa17ed3e7a4
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-01-19 11:04:06 -0600

    comments added
    to all code for readability, future proofing and self understanding

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,22 +1,19 @@
 class CLI
 
     def menu
-        user_input = ""
+        user_input = ""                                               # Initialize variable to store user input
     
-        while user_input != "exit"
-          puts "Request movies Oscar Award Winning movies by year "
-          puts "2015, 2014, 2013, 2012, 2011, 2010"
-          puts "Please enter a year"
-          # get user input
-          user_input = gets.chomp
-          # exit if user input is exit
-          break if user_input == 'exit'
-          # print movies requested by user 
-          Scraper.scrape_and_print_movies(user_input)
+        while user_input != "exit"                                    # Loop until user enters 'exit'
+          puts "Request movies Oscar Award Winning movies by year "   # Print menu options for user
+          puts "2015, 2014, 2013, 2012, 2011, 2010"                   # Print menu options for user
+          puts "Please enter a year"                                  # Print menu options for user          
+          user_input = gets.chomp                                     # get user input          
+          break if user_input == 'exit'                               # exit if user input is exit
+          Scraper.scrape_and_print_movies(user_input)                 # print movies requested by user 
         end
     end
 
-    def run
+    def run                                                           # Run method to start the program
       puts "Greetings"
       menu 
     end
